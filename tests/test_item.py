39ff7235af45c3923a234a421c586b3081ecf6ce
Марат Мусин <austenyt@gmail.com>
2023-07-05 00:01:29 +0300

    Code fixing

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
 from src.item import Item
 
+
 def test_calculate_total_price():
-    assert item1.calculate_total_price(10000, 20) == 200000
-    assert item2.calculate_total_price(20000, 5) == 100000
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.calculate_total_price(), 200000
+    assert item2.calculate_total_price(), 100000
 
 
 def test_apply_discount():
-    assert item1.apply_discount(10000 * 0.8) == 8000.0
-    assert item2.apply_discount(20000 * 0.8) == 16000.0
+    item1 = Item("Смартфон", 10_000, 20)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.apply_discount(), 8_000.0
+
+
+if __name__ == '__main__':
+    pytest.main()
